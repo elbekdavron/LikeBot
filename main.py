bedb1dfb1144345c58adbf65f84ecdb9b796d33a
elbekdavron <elbekd4vronov@gmail.com>
2025-01-25 17:10:04 +0500

    count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 import os
 
 votes = {
@@ -20,6 +20,21 @@ def start(update, context):
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     update.message.reply_photo(photo=photo, reply_markup=reply_markup)
+    
+def count(update, context):
+    
+    if update.callback_query.data == "like":
+        votes["likes"] += 1
+    elif update.callback_query.data == "dislike":
+        votes["dislikes"] += 1
+
+    keyboard = [
+        [InlineKeyboardButton(f"👍 Like {votes['likes']}", callback_data="like")],
+        [InlineKeyboardButton(f"👎 Dislike {votes['dislikes']}", callback_data="dislike")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.callback_query(reply_markup=reply_markup) 
 
 
 updater = Updater(TOKEN)  
@@ -27,6 +42,8 @@ dispatcher = updater.dispatcher
 
 
 dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CallbackQueryHandler(count))
+
 
 updater.start_polling()
 updater.idle()
